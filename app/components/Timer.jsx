be616a1fc0cc928fa114e052814754218145891c
Eric Bell <eric@polymorph.co>
2016-08-09 06:09:09 -0400

    68 adding routing and menu styling

---
+++ app/components/Timer.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Timer = React.createClass({
+    displayName: 'Timer',
+    render() {
+        return (
+            <div>Timer</div>
+        );
+    }
+});
+
+module.exports = Timer;
